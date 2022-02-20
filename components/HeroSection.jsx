c006f84fd691505eef7179f32f071adf9af886a0
Nie-Mand <chill.out.x19@gmail.com>
2022-02-20 05:28:16 +0100

    fixed hero

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -1,12 +1,17 @@
-import Navbar from './Navbar'
-import Images from './Images'
+import Navbar from "./Navbar"
+import Images from "./Images"
 
 const HeroSection = () => {
   return (
-    <div>
-      <Navbar/>
-      <Images/>
-      <div className='flex justify-center my-3'><button className='py-3 px-8 bg-black text-white rounded-full z-0 hover:bg-black/80 duration-200'>Reserver dans Lounge</button></div>
+    <div className="h-screen">
+      <Navbar />
+      <div className="h-20"></div>
+      <Images />
+      <div className="w-full grid place-content-center">
+        <button className="mx-auto w-52 h-10 bg-black text-white rounded-full z-0 hover:bg-black/80 duration-200">
+          Reserver une Table
+        </button>
+      </div>
     </div>
   )
 }
