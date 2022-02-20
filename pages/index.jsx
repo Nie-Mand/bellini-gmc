2c606626206704ec09a6bb89dbcf9ca81b967543
crymfox <sameur.bh@gmail.com>
2022-02-20 03:02:20 -0800

    yea

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,20 @@
 import Navbar from '../components/Navbar'
+import Image from 'next/images';
+import {bgWrap} from '../styles.module.css'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
+      <div className={bgWrap}>
+        <Image
+          alt="travel"
+          src="../public/img/mainBg.png"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+        />
+      </div>
     </div>
   )
 }
