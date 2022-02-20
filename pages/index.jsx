97f4538fae8c7873a7bc99009eac86dce54fed41
crymfox <sameur.bh@gmail.com>
2022-02-20 05:32:52 -0800

    yes

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,6 @@
-import Navbar from "../components/Navbar"
-import Image from "next/image"
-import { bgWrap } from "../styles.module.css"
 import Booking from "../components/Booking"
 import HeroSection from "../components/HeroSection"
+
 const Home = () => {
   return (
     <div>
