0f696e57139e31d37d693a313cb250ea375d7b11
crymfox <sameur.bh@gmail.com>
2022-02-20 09:10:27 -0800

    yea

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Booking from "../components/Booking"
 import HeroSection from "../components/HeroSection"
 import Gallery from "../components/Gallery"
+import Footer from "../components/Footer"
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Gallery />
       <div className=" grid h-auto bg-slate-200"><div className="h-32"></div></div>
       <Booking />
+      <Footer />
     </div>
   )
 }
