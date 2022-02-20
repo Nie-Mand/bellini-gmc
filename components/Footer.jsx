68425dda9aa721cbc58d94bdd0c32336dc996be9
crymfox <sameur.bh@gmail.com>
2022-02-20 10:26:10 -0800

    Yay

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className=" h-1/2">
                 <div className=" flex">
                     <div className=" flex md:flex-row flex-col text-base text-[#a0aec0] md:gap-14 gap-2 mb-10 md:mt-10 mt-8 md:w-auto w-full">
-                        <button className=" hover:text-white duration-200 font-semibold flex justify-start"><FontAwesomeIcon icon={faInstagram} /><span className='px-4'>Instagram</span></button>
+                        <a href='https://www.instagram.com/bellinisousse/' className=" hover:text-white duration-200 font-semibold flex justify-start"><FontAwesomeIcon icon={faInstagram} /><span className='px-4'>Instagram</span></a>
                         <div className={` bg-[#222529] md:w-0 w-full md:absolute md:invisible h-[0.25px]`}></div>
                         <button className=" hover:text-white duration-200 font-semibold flex justify-start"><FontAwesomeIcon icon={faWhatsapp} /><span className='px-4'>WhatsApp</span></button>
                         <div className={` bg-[#222529] md:w-0 w-full md:absolute md:invisible h-[0.25px]`}></div>
